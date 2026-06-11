e16fdec8896d37f65627f50eca40519c17be01d8
dong lee <vietb415@gmail.com>
2026-06-11 23:46:06 +0700

    [Feat]: Khởi tạo Area Admin và cấu hình định tuyến Area trong Program

--- THAN_NONG_SHOP/Program.cs
+++ THAN_NONG_SHOP/Program.cs
@@ -32,6 +32,11 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
