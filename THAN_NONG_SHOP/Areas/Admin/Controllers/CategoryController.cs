14592716cd0cb480c4ba91dbdedef7828160e34d
dong lee <vietb415@gmail.com>
2026-06-12 01:44:22 +0700

    [Fix]: Dọn dẹp thẻ thừa trong Admin Layout và hoàn thiện trang hiển thị danh mục

---
+++ THAN_NONG_SHOP/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using THAN_NONG_SHOP.Data;
+
+namespace THAN_NONG_SHOP.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly THAN_NONG_SHOP_DbContext _db;
+
+        public CategoryController(THAN_NONG_SHOP_DbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _db.Categories.ToList();
+            return View(categories);
+        }
+}
+}
