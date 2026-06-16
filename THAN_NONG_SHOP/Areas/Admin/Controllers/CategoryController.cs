25acdfdf3fa99472756180975296fa20732ecaf8
dong lee <vietb415@gmail.com>
2026-06-16 20:47:58 +0700

    [Feature]: Hoàn thành trang danh sách sản phẩm hiển thị ảnh và danh mục chuẩn Eager Loading

--- THAN_NONG_SHOP/Areas/Admin/Controllers/CategoryController.cs
+++ THAN_NONG_SHOP/Areas/Admin/Controllers/CategoryController.cs
@@ -39,5 +39,56 @@ namespace THAN_NONG_SHOP.Areas.Admin.Controllers
 
 
         }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            var categories = _db.Categories.Find(id);
+            if(categories == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0) return NotFound();
+            var Categories = _db.Categories.Find(id);
+            if (Categories == null) return NotFound();
+
+            return View(Categories);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int id)
+        {
+            var categories = _db.Categories.Find(id);
+            if (categories == null) { return NotFound(); }
+            _db.Categories.Remove(categories);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
 }
 }
---
+++ THAN_NONG_SHOP/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using THAN_NONG_SHOP.Data;
+using THAN_NONG_SHOP.Models;
+
+namespace THAN_NONG_SHOP.Areas.Admin.Controllers
+{
+  
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+
+
+        private readonly THAN_NONG_SHOP_DbContext _db;
+
+        public ProductController(THAN_NONG_SHOP_DbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var products = _db.Products.Include(p => p.Category).ToList();
+            return View(products);
+        }
+    }
+}
